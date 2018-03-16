531ded6b9d0eec900a801404fda2a35348f81ee5
Jan <cppvoid@safe-mail.net>
2018-03-16 10:57:18 +0100

    marco move

--- src/SuperMarco.java
+++ src/SuperMarco.java
@@ -1,9 +1,13 @@
 import org.newdawn.slick.*;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SuperMarco extends BasicGame {
 
     private Image marco;
     private Image wood;
+    private AtomicInteger marcoPosX = new AtomicInteger(50);
+
 
     public SuperMarco(String gameName) {
         super(gameName);
@@ -15,20 +19,27 @@ public class SuperMarco extends BasicGame {
             marco = new Image("media/pictures/marco.png");
             wood = new Image("media/pictures/wood.png");
         } catch (SlickException e) {
-            System.out.println(e.getMessage() + "lol");
+            System.out.println(e.getMessage());
         }
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
         if(gameContainer.getInput().isKeyDown(Input.KEY_ESCAPE)) {
             gameContainer.exit();
+        }
+        if(gameContainer.getInput().isKeyDown(Input.KEY_RIGHT)) {
+            marcoPosX.updateAndGet((x) -> x + 1);
+        }
+        if(gameContainer.getInput().isKeyDown(Input.KEY_LEFT)) {
+            marcoPosX.updateAndGet((x) -> x - 1);
         }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        marco.draw(50, 50);
+        System.out.println(marcoPosX);
+        marco.draw(marcoPosX.floatValue(), 50);
         wood.draw(25, 200);
     }
 
@@ -37,6 +48,7 @@ public class SuperMarco extends BasicGame {
             AppGameContainer container = new AppGameContainer(new SuperMarco("SuperMarco"));
             container.setDisplayMode(800, 600, false);
             container.start();
+            container.setTargetFrameRate(30);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
